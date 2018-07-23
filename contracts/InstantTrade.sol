4649247dfc730bcc6075682d11746b3671ac78be
TokenDev <41195637+TokenDev@users.noreply.github.com>
2018-07-23 03:49:54 +0200

    Minor contract cleanup

--- contracts/InstantTrade.sol
+++ contracts/InstantTrade.sol
@@ -15,7 +15,7 @@ contract ZeroExchange {
         bytes32 s)
         public {}
         
-    function fillOrder(
+  /*  function fillOrder(
           address[5] orderAddresses,
           uint[6] orderValues,
           uint fillTakerTokenAmount,
@@ -26,30 +26,32 @@ contract ZeroExchange {
           public
           returns (uint filledTakerTokenAmount)
           {}
-    
+    */
 }
 
 contract WETH {
- 
     function deposit() public payable {}
-    
     function withdraw(uint wad) public {}
 }
 
 contract InstantTrade is SafeMath, Ownable {
 
-    address public Weth;
-    address public ZeroX;
-    address public ProxyZeroX;
-
+    address public wETH;
+    address public zeroX;
+    address public proxyZeroX;
+    
    constructor(address _weth, address _zeroX) Ownable() public {
-       Weth = _weth;
-       ZeroX = _zeroX;
-       ProxyZeroX = ZeroExchange(ZeroX).TOKEN_TRANSFER_PROXY_CONTRACT();
+       wETH = _weth;
+       zeroX = _zeroX;
+       proxyZeroX = ZeroExchange(zeroX).TOKEN_TRANSFER_PROXY_CONTRACT();
    }
-
+   
   // This is needed so we can withdraw funds from other smart contracts
   function() public payable {
+  }
+  
+  function getTotalFeeValue(uint _amount) internal returns (uint) {
+      return safeMul(_amount, 1004) / 1000;
   }
   
   // End to end trading in a single call (Token Store, EtherDelta)
@@ -57,7 +59,7 @@ contract InstantTrade is SafeMath, Ownable {
       uint _expires, uint _nonce, address _user, uint8 _v, bytes32 _r, bytes32 _s, uint _amount, address _store) external payable {
     
     // Fix max fee (0.4%) and always reserve it
-    uint totalValue = safeMul(_amount, 1004) / 1000;
+    uint totalValue = getTotalFeeValue(_amount);
     
     // Paying with Ethereum or token? Deposit to the actual store
     if (_tokenGet == address(0)) {
@@ -67,24 +69,24 @@ contract InstantTrade is SafeMath, Ownable {
     } else {
       require(msg.value == 0);
         
-      // Assuming user already approved transfer, transfer first to this contract
+      // Assuming user already approved transfer, transfer to this contract
       require(Token(_tokenGet).transferFrom(msg.sender, this, totalValue));
 
-      // Allow now actual store to deposit
+      // Deposit token to the exchange
       require(Token(_tokenGet).approve(_store, totalValue)); 
       TokenStore(_store).depositToken(_tokenGet, totalValue);
     }
     
-    // Trade
     /*
     TokenStore(_store).trade(_tokenGet, _amountGet, _tokenGive, _amountGive,
       _expires, _nonce, _user, _v, _r, _s, _amount);
     */
 
-    // Wrap trade function in a call to avoid a 'throw;' using up all gas.
+    // Wrap trade function in a call to avoid a 'throw;'(EtherDelta) using up all gas, returns (bool success)
     require(
         address(_store).call(
-          bytes4(keccak256("trade(address,uint256,address,uint256,uint256,uint256,address,uint8,bytes32,bytes32,uint256)")),
+          bytes4(0x0a19b14a), // precalculated Hash of the line below
+          //bytes4(keccak256("trade(address,uint256,address,uint256,uint256,uint256,address,uint8,bytes32,bytes32,uint256)")),  
           _tokenGet, _amountGet, _tokenGive, _amountGive,_expires, _nonce, _user, _v, _r, _s, _amount
         )
     );
@@ -115,53 +117,52 @@ contract InstantTrade is SafeMath, Ownable {
       && _orderValues[3] == 0
     ); 
     
-
+    
     // Fix max fee (0.4%) and always reserve it
-    uint totalValue = safeMul(_amount, 1004) / 1000;
+    uint totalValue = getTotalFeeValue(_amount);
     
     // Paying with (wrapped) Ethereum or  other token? 
-    if (/*takerToken*/_orderAddresses[3] == Weth) {
+    if (/*takerToken*/_orderAddresses[3] == wETH) {
+        
       // Check amount of ether sent to make sure it's correct
       require(msg.value == totalValue);
        // Convert to wrapped ETH
-      WETH(Weth).deposit.value(msg.value)();
-      require(Token(Weth).approve(ProxyZeroX, msg.value)); 
+      WETH(wETH).deposit.value(msg.value)();
+      require(Token(wETH).approve(proxyZeroX, msg.value)); 
     } else {
         
       require(msg.value == 0);
       
-      // Assuming user already approved transfer, transfer first to this contract
+      // Assuming user already approved transfer, transfer to this contract
       require(Token(/*takerToken*/_orderAddresses[3]).transferFrom(msg.sender, this, totalValue));
 
       // Allow now actual store to deposit
-      require(Token(/*takerToken*/_orderAddresses[3]).approve(ProxyZeroX, totalValue)); 
+      require(Token(/*takerToken*/_orderAddresses[3]).approve(proxyZeroX, totalValue)); 
     } 
     
     // Trade and require that it was the full amount
-    ZeroExchange(ZeroX).fillOrKillOrder(_orderAddresses, _orderValues, _amount, _v, _r, _s);
+    ZeroExchange(zeroX).fillOrKillOrder(_orderAddresses, _orderValues, _amount, _v, _r, _s);
 
-    
     // Check how much did we get and how much should we send back
-    totalValue = Token(_orderAddresses[2]).balanceOf(this);
     uint customerValue = safeMul(/*makerTokenAmount*/_orderValues[0], _amount) / /*takerTokenAmount*/_orderValues[1];
     
     // Now unwrap funds and send to user
-    if (/*makerToken*/_orderAddresses[2] == Weth) {
-      WETH(Weth).withdraw(totalValue);
+    if (/*makerToken*/_orderAddresses[2] == wETH) {
+      totalValue = Token(wETH).balanceOf(this);
+      WETH(wETH).withdraw(totalValue);
       msg.sender.transfer(customerValue);
     } else {
       require(Token(_orderAddresses[2]).transfer(msg.sender, customerValue));
-    } 
-    
+    }  
   } 
   
- 
+  // Withdraw funds earned from fees
   function withdrawFees(address _token) external onlyOwner {
     if (_token == address(0)) {
       msg.sender.transfer(address(this).balance);
     } else {
-      uint amount = Token(_token).balanceOf(address(this));
-      require(Token(_token).transfer(msg.sender, amount));
+      Token token = Token(_token);
+      require(token.transfer(msg.sender, token.balanceOf(address(this))));
     }
   }  
 }
